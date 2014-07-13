6cf69294b353ede26b824e61f4384b5e77f81018
unknown <ccove@SC-2300-12.labs.surrey.sfu.ca>
2014-07-13 14:14:25 -0700

    basic render layers have been implemented

--- superParty/src/rendering/rendering.js
+++ superParty/src/rendering/rendering.js
@@ -1,27 +1,114 @@
-var Rendering 		= {};
+var Rendering 			= {};
+setupRenderingMethods(Rendering);
 Rendering.renderers 	= {};
-Rendering.config = 
-	{
-		visible:true,
-		layer:0,
-		transform:
-		{
-			position:{x:0, y:0}, 
-			velocity: {x:0, y:0}, 
-			rotation: 0, 
-			scale:1
-		}
-	};
-Rendering.register = function(name, renderFactory)
+Rendering.layers 		= [];
+
+Looper.addEventListener(Looper.EVENT_DRAW_TICK, Rendering.doRender);
+
+
+//-------------------------
+//Standard Layers
+//-------------------------
+var RenderLayers = {};
+
+//-------------------------
+//Setup
+//-------------------------
+
+Rendering.addRenderLayer(-10, 'BACKGROUND');
+Rendering.addRenderLayer(0, 'MAIN');
+Rendering.addRenderLayer(10, 'FORGROUND');
+
+function setupRenderingMethods(Rendering)
 {
-	this.renderers[name] 			= renderer;
-}
-Rendering.getRenderer = function(name)
-{	
-	//return if there is no render factory for given name
-	if(!Rendering.renderers[name]) return;
-	
-	var renderer = $.extend( true, Rendering.config, Rendering.renderers[name]());
-	
-	return renderer;
+	Rendering.rendererConfig = 
+		{
+			draw:function(drawInfo){},
+			visible:true,
+			layer:0,
+			transform:
+			{
+				position:{x:0, y:0}, 
+				velocity: {x:0, y:0}, 
+				rotation: 0, 
+				scale:1
+			}
+		};
+	Rendering.doRender = function(e)
+	{
+		for (var i in Rendering.layers)
+		{
+			var layer = Rendering.layers[i];
+			layer.render({});
+		}
+	}
+	Rendering.register = function(name, renderFactory)
+	{
+		this.renderers[name] 			= renderer;
+	}
+	Rendering.getRenderer = function(name)
+	{	
+		//return if there is no render factory for given name
+		if(!Rendering.renderers[name]) return;
+		
+		var renderer = $.extend( true, Rendering.rendererConfig, Rendering.renderers[name]());
+		
+		return renderer;
+	}
+	Rendering.addRenderLayer = function(index, name)
+	{	
+		var newLayer = {index:index, list:[]};
+		
+		var listIndex = 0;
+		
+		//find ordered list index of the desired layer index
+		for(var i=0;i<this.layers.length;i++)
+		{
+			var layer = this.layers[i];
+			if(layer.index>=newLayer.index)break;
+			listIndex = i;
+		}
+		
+		var listSize = this.layers.length;
+		
+		//shift layers over in the list for fit the new layer. 
+		for(var i = listIndex;i<listSize;i++)
+		{
+			this.layers[i+1] = this.layers[i];
+		}
+		
+		this.layers[listIndex] = newLayer;
+		
+		RenderLayers[name] = newLayer;
+		
+		this.configNewLayer(newLayer);
+		
+		return newLayer;
+	}
+	Rendering.removeRenderLayer = function(name)
+	{	
+		if(RenderLayers[name] == null)return;
+		
+		removeFromList(this.layers, RenderLayers[name]);
+		
+		RenderLayers[name] = null;
+	}
+	Rendering.configNewLayer = function(layer)
+	{
+		layer.add = function(renderer)
+		{
+			this.list.push(renderer);
+		}
+		layer.remove = function(renderer)
+		{
+			removeFromList(this.list, renderer);
+		}
+		layer.render = function(drawInfo)
+		{
+			for(var i=0;i<this.list.length;i++)
+			{
+				this.list[i].draw(drawInfo);
+			}
+		}
+	}
 }
