9984d040814887e396ff45ac882821fecf4cbb54
unknown <ccove@SC-2300-08.labs.surrey.sfu.ca>
2014-07-27 13:59:48 -0700

    starting basic prototype with current engine implementation. All missing components will be faked in the prototype folder. Monster with player control currently implemented

--- MySuperGameDemo/src/MySuperGameDemo.js
+++ MySuperGameDemo/src/MySuperGameDemo.js
@@ -4,31 +4,20 @@ SuperParty.onSetupComplete = doSetup;
 
 function doSetup()
 {
-	SuperParty.loadScripts(init, ['MySuperGameDemo/src/DemoLevel.js']);
+	SuperParty.loadScripts(init, ['MySuperGameDemo/src/DemoLevel.js', 'MySuperGameDemo/src/monsterBasic.js']);
 }
 function init()
 {
 	Resources.addEventListener(Resources.EVENT_ON_COMPELETE, startGame);
 	
 	Resources.addImage('testImage', 'MySuperGameDemo/res/3268850-master+chief+epicness.jpg');
+	Resources.addImage('monster', 'MySuperGameDemo/res/microbe.png');
 	
 	Resources.startLoad();
 }
 function startGame(e)
 {
-
-    var directionInput = Input.getStandardDirectionInput({wasd:true});
-
-    var x=200;
-    var y=200;
-
-    var speed = 3;
-
-    directionInput.up.onPress       = function(){};
-    directionInput.down.onPress     = function(){};
-    directionInput.left.onPress     = function(){};
-    directionInput.right.onPress    = function(){};
-
+	setupLevel();
 	var canvas 	= Stage.canvas;
     var context = Stage.superContext;
 
@@ -38,60 +27,29 @@ function startGame(e)
 	Physics.startDebugDraw();
 	
 	var testBody  = Physics.bodies.getCircle({radius:50,mass:5, isTrigger:false});
-	var charBody  = Physics.bodies.getCircle({radius:5, isTrigger:false, damp:0.2});
 	var triggerBody  = Physics.bodies.getCircle({radius:20, isTrigger:true});
 	
-	charBody.transform.position.x = x;
-	charBody.transform.position.y = y;
+	var userMonster = createUserMonster();
+	
+	userMonster.body.transform.position.x = 200;
+	userMonster.body.transform.position.y = 200;
 	
 	triggerBody.transform.position.x = 100;
 	triggerBody.transform.position.y = 200;
 	
 	function draw()
     {
-		x = charBody.transform.position.x;
-		y = charBody.transform.position.y;
-		
 		//reset canvas
 		Stage.context.rect(0,0,canvas.width,canvas.height);
 		context.setFillStyle="black";
 		context.fill();
-		
-		
+
         context.drawImage(R.drawable.testImage, 0, 0, 500, 500);
-		
-        context.beginPath();
-        context.moveTo(x, y);
-
-        context.lineTo(30+x, 30+y);
-		context.setLineWidth(10);
-
-		context.setStrokeStyle('#ff0000');
-		context.stroke();
     }
     function update()
     {
-		//charBody.transform.setVelocity(0, 0);
-        //movement
-        if(directionInput.up.isDown)
-        {
-			charBody.transform.setVelocity(0, -speed);
-            y-=speed;
-        }
-          if(directionInput.down.isDown)
-        {
-            charBody.transform.setVelocity(0, speed);
-        }
-        if(directionInput.right.isDown)
-        {
-           charBody.transform.setVelocity(speed, 0);
-        }
-        if(directionInput.left.isDown)
-        {
-            charBody.transform.setVelocity(-speed, 0);
-        }
-        Stage.cam.x += ((x-canvas.width/2)-Stage.cam.x)/10;
-        Stage.cam.y += ((y-canvas.height/2)-Stage.cam.y)/10;
+        Stage.cam.x += ((userMonster.body.transform.position.x-canvas.width/2)-Stage.cam.x)/10;
+        Stage.cam.y += ((userMonster.body.transform.position.y-canvas.height/2)-Stage.cam.y)/10;
 		
 		
     }
